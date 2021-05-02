194bb30622cb4d057332366025cbf8826f500a2c
karimkohel <kareemkohel@gmail.com>
2021-05-02 17:01:06 +0200

    done with client socket test with buffer

--- potato.py
+++ potato.py
@@ -1,1 +1,15 @@
-# this is the main client app
+import socket
+
+HEADERSIZE = 20
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+clientSocket.connect((socket.gethostname(), 5000))
+
+msg = "Hello there nourhan osama"
+
+data = f'{len(msg):<{HEADERSIZE}}'+ msg
+
+codedMsg = bytes(data, "utf-8")
+
+
+clientSocket.send(codedMsg)
