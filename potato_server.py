563bd0d903d08c25df6405ac2d407328cbb99582
karimkohel <kareemkohel@gmail.com>
2021-05-02 23:00:04 +0200

    used the newly added socket fx to clean up main file

--- potato_server.py
+++ potato_server.py
@@ -1,36 +1,9 @@
 import socket
+import botsocks
 
-HEADERSIZE = 20
-
-serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-serverSocket.bind((socket.gethostname(), 5000))
-serverSocket.listen(5)
+serverSocket = botsocks.startConnection(socket.gethostname(), 5000)
 
 while True:
 
-    clientSocket, address = serverSocket.accept()
-    print(f"connection from : {address}")
-    fullMsg = ''
-    newMsg = True
-
-    while True:
-
-        msg = clientSocket.recv(HEADERSIZE+5)
-
-        # if msg start then get it's len from the header
-        if newMsg:
-            decodedMsg = msg[:HEADERSIZE].decode("utf-8")
-            msgLen = int(decodedMsg)
-            newMsg = False
-
-        fullMsg += msg.decode("utf-8")
-
-        if len(fullMsg)-HEADERSIZE == msgLen:
-            print(fullMsg[HEADERSIZE:])
-            newMsg = True
-            break
-        
-
-
-    clientSocket.close()
+    msg = botsocks.getMessage(serverSocket)
+    print(msg)
