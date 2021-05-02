50659aa436d01428fe0ebdf0be7d929e57fbf5af
karimkohel <kareemkohel@gmail.com>
2021-05-02 23:28:34 +0200

    started basic main file of assistant bot

--- potato_server.py
+++ potato_server.py
@@ -1,9 +1,16 @@
 import socket
 import botsocks
+import neuralintents
+from functions import mappings
 
-serverSocket = botsocks.startConnection(socket.gethostname(), 5000)
+# serverSocket = botsocks.startConnection(socket.gethostname(), 5000)
+bot = neuralintents.GenericAssistant("intents.json", mappings, "model")
+bot.train_model()
+# bot.save_model()
+# bot.load_model()
 
 while True:
 
-    msg = botsocks.getMessage(serverSocket)
+    # msg = botsocks.getMessage(serverSocket)
+    msg = "hey there"
     print(msg)
