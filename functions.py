9fa5f7a089898bc8e69dfb1a0a78df3ecad04bb2
karimkohel <kareemkohel@gmail.com>
2021-05-02 17:32:49 +0200

    start and doc for functions file

---
+++ functions.py
@@ -0,0 +1,12 @@
+import speech
+
+"""
+Basic setup of a function
+
+def function(intent):
+    speech.speak(intent + data)
+
+where intent is the bot response to a given command
+and the data is the task you got in the function
+
+"""
