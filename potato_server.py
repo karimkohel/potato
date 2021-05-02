9d475acf9d4f11a3e4ff0a4c953e13bc1a1c2950
karimkohel <kareemkohel@gmail.com>
2021-05-02 16:57:54 +0200

    working socketed server with no buffer

--- potato_server.py
+++ potato_server.py
@@ -1,1 +1,17 @@
-# this is the main server app
+import socket
+
+HEADERSIZE = 20
+
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serverSocket.bind((socket.gethostname(), 5000))
+serverSocket.listen(5)
+
+while True:
+
+    clientSocket, address = serverSocket.accept()
+    print(f"connection from : {address}")
+    data = clientSocket.recv(30)
+    msg = data.decode("utf-8")
+    print(msg)
+    clientSocket.close()
