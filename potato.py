715fee8a4139d84402ac7cab7908f49e939d5026
Nourhan <nourhanelyamany@gmail.com>
2021-05-05 22:11:32 +0200

    adding label into the application windows

--- potato.py
+++ potato.py
@@ -1,16 +1,24 @@
 import socket
 from usersock import ClientSock
-import tkinter
+import tkinter as tk
 from tkinter import *
 
 # construct clientSock class and bind ip and port with socket
 client = ClientSock(socket.gethostbyname(socket.gethostname()),5000)
 client.connect()
 
-potatoWindow = Tk()
+potatoWindow = tk.Tk()
 potatoWindow.geometry('800x600')
 potatoWindow.title("potato is here")
-
+#canvas= Canvas(potatoWindow)
+label = tk.Label(
+    text="Hello, I'm your virtual assistant for today :) ",
+    foreground="white",  # Set the text color to white
+    background="green",  # Set the background color to black
+    width=200,
+    height=200
+)
+label.pack()
 while True:
 
     msg = input("Enter msg to potato: ")
