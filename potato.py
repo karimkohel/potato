d326130f4cfd4719a93a80a379f51288dfa665ae
Nourhan <nourhanelyamany@gmail.com>
2021-05-05 21:27:17 +0200

    opening application window

--- potato.py
+++ potato.py
@@ -1,9 +1,15 @@
 import socket
 from usersock import ClientSock
+import tkinter
+from tkinter import *
 
 # construct clientSock class and bind ip and port with socket
 client = ClientSock(socket.gethostbyname(socket.gethostname()),5000)
 client.connect()
+
+potatoWindow = Tk()
+potatoWindow.geometry('800x600')
+potatoWindow.title("potato is here")
 
 while True:
 
@@ -12,3 +18,5 @@ while True:
     client.sendMsg(msg)
     response = client.recvMsg()
     print(response)
+
+potatoWindow.mainloop()
