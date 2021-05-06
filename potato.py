106c73cd4bf5c73a1b83646194f2a0bcb51953dc
Nourhan <nourhanelyamany@gmail.com>
2021-05-07 00:08:53 +0200

    done with frame 1 gui with widgets dictionary

--- potato.py
+++ potato.py
@@ -9,10 +9,12 @@ from PyQt5.QtGui import QCursor, QIcon, QPixmap, QFont
 # client.connect()
 
 #initiallize GUI application
+widgets = {
+      "images" : [],
+      "buttons" : []
+}
 potatoApp = QApplication(sys.argv)
 grid = QGridLayout()
-
-
 #window and settings
 potatoWindow = QWidget()
 potatoWindow.setWindowTitle("Hey, potato is here!")
@@ -20,36 +22,42 @@ potatoWindow.setFixedSize(500,600)
 potatoWindow.move(4000, 200)               #position of the window
 potatoWindow.setStyleSheet("background: #FFFDD0;")
 
-#display logo
-helloPotato = QPixmap("hello.png")
-welcomeImg = QLabel()
-welcomeImg.setPixmap(helloPotato)
-welcomeImg.setAlignment(QtCore.Qt.AlignCenter)
-welcomeImg.setStyleSheet("margin-top: 200px;")
+def frameOne():
+      #display logo
+      helloPotato = QPixmap("hello.png")
+      welcomeImg = QLabel()
+      welcomeImg.setPixmap(helloPotato)
+      welcomeImg.setAlignment(QtCore.Qt.AlignCenter)
+      welcomeImg.setStyleSheet("margin-top: 200px;")
+      widgets["images"].append(welcomeImg)
 
-#add button
-startButton = QPushButton("ask potato..")
-startButton.setGeometry(200, 150, 100, 30)
-startButton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
-startButton.setStyleSheet(
-      "*{border: 2px solid '#E1865F';" +
-      "border-radius: 13px;" +
-      "background: '#E1865F';" +
-      "font-size: 15px;" +
-      "color: '#132733';" +
-      "padding: 10px 0;" +
-      "margin-top: 500px;" +
-      "margin: 150px 150px;}" +
-      "*:hover{background: '#D35A26';}" 
-)
-#add widgets to the grid
-grid.addWidget(welcomeImg,0,0)
-grid.addWidget(startButton, 0,0)
+      #add button
+      startButton = QPushButton("ask potato..")
+      startButton.setGeometry(200, 150, 100, 30)
+      startButton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+      startButton.setStyleSheet(
+            "*{border: 2px solid '#E1865F';" +
+            "border-radius: 13px;" +
+            "background: '#E1865F';" +
+            "font-size: 15px;" +
+            "color: '#132733';" +
+            "padding: 10px 0;" +
+            "margin-top: 500px;" +
+            "margin: 150px 150px;}" +
+            "*:hover{background: '#D35A26';}" 
+
+      )
+      widgets["buttons"].append(startButton)
+
+      #add widgets to the grid
+      grid.addWidget(widgets["images"][-1],0,0)
+      grid.addWidget(widgets["buttons"][-1], 0,0)
+
+frameOne()
 
 potatoWindow.setLayout(grid)
 potatoWindow.show()
 sys.exit(potatoApp.exec())
-# while True:
 
 #     msg = input("Enter msg to potato: ")
 
