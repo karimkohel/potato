50cc8b1ef6c91eb128898718a4f54ead430232fd
Nourhan <nourhanelyamany@gmail.com>
2021-05-06 23:55:31 +0200

    pyQt5 gui trial

--- potato.py
+++ potato.py
@@ -1,34 +1,54 @@
 # import socket
 # from usersock import ClientSock
-import tkinter as tk
-from tkinter import *
-from PIL import ImageTk,Image
-
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QCursor, QIcon, QPixmap, QFont
 # construct clientSock class and bind ip and port with socket
 # client = ClientSock(socket.gethostbyname(socket.gethostname()),5000)
 # client.connect()
-WIN_WIDTH = 500
-WIN_HEIGHT = 600
-potatoWindow = tk.Tk()
-potatoWindow.geometry('500x600')
-potatoWindow.resizable(False,False)
-potatoWindow.title("potato is here")
-canvas = Canvas(potatoWindow,width = 500,height = 600, background = 'red')
+
+#initiallize GUI application
+potatoApp = QApplication(sys.argv)
+grid = QGridLayout()
 
 
-img = ImageTk.PhotoImage(Image.open("hello.png"))  
-canvas.create_image(0, 0, anchor=CENTER, image=img, tags="bg_img")
+#window and settings
+potatoWindow = QWidget()
+potatoWindow.setWindowTitle("Hey, potato is here!")
+potatoWindow.setFixedSize(500,600)
+potatoWindow.move(4000, 200)               #position of the window
+potatoWindow.setStyleSheet("background: #FFFDD0;")
 
-label = tk.Label(
-    text="Hello, I'm your virtual assistant for today :) ",
-    foreground="green",  # Set the text color to white
-    background = "#ffe5b4",
-    width= int(WIN_WIDTH/2),
-    height= int(WIN_HEIGHT/0.7),
-    font=("Symbol", 16)   
+#display logo
+helloPotato = QPixmap("hello.png")
+welcomeImg = QLabel()
+welcomeImg.setPixmap(helloPotato)
+welcomeImg.setAlignment(QtCore.Qt.AlignCenter)
+welcomeImg.setStyleSheet("margin-top: 200px;")
+
+#add button
+startButton = QPushButton("ask potato..")
+startButton.setGeometry(200, 150, 100, 30)
+startButton.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+startButton.setStyleSheet(
+      "*{border: 2px solid '#E1865F';" +
+      "border-radius: 13px;" +
+      "background: '#E1865F';" +
+      "font-size: 15px;" +
+      "color: '#132733';" +
+      "padding: 10px 0;" +
+      "margin-top: 500px;" +
+      "margin: 150px 150px;}" +
+      "*:hover{background: '#D35A26';}" 
 )
-label.pack()
-canvas.pack()
+#add widgets to the grid
+grid.addWidget(welcomeImg,0,0)
+grid.addWidget(startButton, 0,0)
+
+potatoWindow.setLayout(grid)
+potatoWindow.show()
+sys.exit(potatoApp.exec())
 # while True:
 
 #     msg = input("Enter msg to potato: ")
@@ -36,5 +56,3 @@ canvas.pack()
 #     client.sendMsg(msg)
 #     response = client.recvMsg()
 #     print(response)
-
-potatoWindow.mainloop()
