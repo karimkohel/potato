c7cdc076dc8b10254260eedc761a42d09c6c4450
Mohameosama <moandosama1163@gmail.com>
2021-05-11 01:07:57 +0200

    edit googlesearch fx

--- client/client_fx.py
+++ client/client_fx.py
@@ -1,29 +1,24 @@
 from speech import speak, takeCommand
+import webbrowser
 
 
-def googleSearch(typingbox = None):
-    if typingbox:
-        search = typingbox.toPlainText()
+def googleSearch(chatObj = None):
+    if chatObj:
+        search = chatObj[0].toPlainText()
+        chatObj[1].append(f"potato: " + "here you go")
     else:
-        search = takeCommand()
-    URl = ("https://www.google.com/search?q=")
-    webbrowser.open(URl + search)
+        pass
+        # search = takeCommand()
+        # speak("here you go")
+
+    URl = "https://www.google.com/search?q=" + search
+    webbrowser.open(URl)
 
 def youtubeSearch():
     search = server.getMessage(client)
     youtubeUrl = ("https://www.youtube.com/results?search_query=")
     server.sendMessage(client, response)
     webbrowser.open(youtubeUrl + search)
-
-
-
-
-
-
-
-
-
-
 
 
 
