da7b88fe130361fd544580c3c6dcb91107bdfb37
karimkohel <kareemkohel@gmail.com>
2021-05-10 18:12:04 +0200

    removed unwanted comments

--- functions.py
+++ functions.py
@@ -9,7 +9,6 @@ def randomRange(response, server, client):
     range1 = server.getMessage(client)
     number = random.randint(0, int(range1))
     server.sendMessage(client, response + " " + str(number), 0)
-    # client.close()
 
 def googleSearch(response, server, client):
     search = server.getMessage(client)
@@ -23,16 +22,6 @@ def youtubeSearch(response, server, client):
     server.sendMessage(client, response)
     webbrowser.open(youtubeUrl + search)
 
-# def time(response, server, client):
-#     server.getMessage(client)
-#     getTime = now.strftime("%H %M %p")
-#     server.sendMessage(client, response + " " + getTime)
-
-# def date(response, server, client):
-#     server.getMessage(client)
-#     getDate = now.strftime("%A ,%B %d, %Y")
-#     server.sendMessage(client, response + " " + getDate)
-
 def getWeather(response, server, client):
     # Need to put Location instead of cairo
     api = 'http://api.openweathermap.org/data/2.5/weather?q=Cairo&appid=f1e62ab85ff8b2eca979678d57a6de2e&units=metric' 
@@ -41,13 +30,14 @@ def getWeather(response, server, client):
         allData = requests.get(api).json()
         weather = allData['weather'][0]['description']
         temp = allData['main']['temp']
-        server.sendMessage(client, response + " " + weather + ", with temperatures around " + str(int(temp)) + " degrees")
+        server.sendMessage(client, response + " " + weather + ", with temperatures around " + str(int(temp)) + " degrees", 0)
 
     except TimeoutError:
-       server.sendMessage(client, response + " internet connection error occured, try again later")
+        server.sendMessage(client, response + " internet connection error occured, try again later", 0)
     except Exception:
-        server.sendMessage(client, response  + " An error occured, try again later")
+        server.sendMessage(client, response  + " An error occured, try again later", 0)
 
 mappings = {
     "random" : randomRange,
+    "weather": getWeather,
 }
