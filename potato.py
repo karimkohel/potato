d977d6d4a3ea9975e8242be8f2042c9ffa6ca4e2
Mohameosama <moandosama1163@gmail.com>
2021-05-10 18:38:33 +0200

    init function flag

--- potato.py
+++ potato.py
@@ -7,13 +7,15 @@ while True:
       client.connect()
 
       while True:
-                  msg = input("Enter msg to potato: ")
-                  client.sendMsg(msg)
-                  response, flag = client.recvMsg()
-                  print(response)
-                  if msg == "exit":
-                        client.close()
-                        exit(0)
-                  if flag == "0":
-                        client.close()
-                        break
+            msg = input("Enter msg to potato: ")
+            client.sendMsg(msg)
+            response, flag = client.recvMsg()
+            print(response)
+            if msg == "exit":
+                  client.close()
+                  exit(0)
+            if flag == "0":
+                  client.close()
+                  break
+            else:
+                  client.flagHandler(flag)
