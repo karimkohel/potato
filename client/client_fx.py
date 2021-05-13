74a4648206e873fb5d4fc3b6130d3fa70c1a46b1
karimkohel <kareemkohel@gmail.com>
2021-05-13 17:16:40 +0200

    commented user functions for easier debugging

--- client/client_fx.py
+++ client/client_fx.py
@@ -5,7 +5,7 @@ import urllib
 import re
 
 
-def googleSearch(chatObj = None):
+def googleSearch(chatObj=None):
     if chatObj:
         search = chatObj[0].toPlainText()
         chatObj[1].append(f"potato: " + "here you go")
@@ -22,27 +22,24 @@ def googleSearch(chatObj = None):
 #     youtubeUrl = ("https://www.youtube.com/results?search_query=")
 #     server.sendMessage(client, response)
 #     webbrowser.open(youtubeUrl + search)
-def playMusic(chatObj = None ):
-    
-    if chatObj:
-        songName = chatObj[0].toPlainText()
-        chatObj[1].append(f"potato: " + "On it")
-    else:
-        pass    
 
-    searchLink = 'https://www.youtube.com/results?search_query={}'.format(songName.replace(" ", "+"))
-    htmlPage = urllib.request.urlopen(searchLink)
-    video_ids = re.findall(r"watch\?v=(\S{11})", htmlPage.read().decode())
-    videoLink = "https://www.youtube.com/watch?v=" + video_ids[0]
-    webbrowser.open(videoLink, new=2)
+# def playMusic(chatObj=None):
     
-        
-    
-   
+#     if chatObj:
+#         songName = chatObj[0].toPlainText()
+#         chatObj[1].append(f"potato: " + "On it")
+#     else:
+#         pass    
 
+#     searchLink = 'https://www.youtube.com/results?search_query={}'.format(songName.replace(" ", "+"))
+#     htmlPage = urllib.request.urlopen(searchLink)
+#     video_ids = re.findall(r"watch\?v=(\S{11})", htmlPage.read().decode())
+#     videoLink = "https://www.youtube.com/watch?v=" + video_ids[0]
+#     webbrowser.open(videoLink, new=2)
+    
 
 mappings = {
     "2" : googleSearch,
-    "3" : youtubeSearch,
-    "4":  playMusic
+    # "3" : youtubeSearch,
+    # "4":  playMusic
 }
