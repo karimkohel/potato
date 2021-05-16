3f6e1ab586ee5bb423be22eb401e93c85665307f
Omar Tamer <omar85zaki@gmail.com>
2021-05-16 02:08:20 +0200

    making function for downloading music in client

--- client/client_fx.py
+++ client/client_fx.py
@@ -2,6 +2,17 @@
 import webbrowser
 import urllib
 import re
+from pytube import YouTube
+import os
+import json
+
+try:
+    with open("settings.json") as f:
+        settings = json.load(f)
+except Exception:
+    settings = {'speech_speed': 170, 'voice_number': 0, 'music_folder': ''}
+    with open('settings.json', 'w') as f:
+            json.dump(settings, f)
 
 
 def googleSearch(response):
@@ -9,10 +20,19 @@ def googleSearch(response):
 
 def youtubeSearch(response):
     webbrowser.open(response)
+
+def downloadMusic(response): # handeling the pass but its done
+    video = YouTube(response)
+    print(response)
+    audio = video.streams.last()
+    musicPath = os.path.expanduser("D:\IMages\RED")
+    audio.download(musicPath)
     
+
+
 
 mappings = {
     "2" : googleSearch,
     "3" : youtubeSearch,
-    # "4":  playMusic
+    "4" : downloadMusic
 }
