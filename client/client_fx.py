d3a9ab05ab8b7e46cfca2beb22e59516b74aa83b
karimkohel <kareemkohel@gmail.com>
2021-05-17 00:47:00 +0200

    repaired download music

--- client/client_fx.py
+++ client/client_fx.py
@@ -6,14 +6,16 @@ from pytube import YouTube
 import os
 import json
 
-try:
-    with open("settings.json") as f:
-        settings = json.load(f)
-except Exception:
-    settings = {'speech_speed': 170, 'voice_number': 0, 'music_folder': ''}
-    with open('settings.json', 'w') as f:
-            json.dump(settings, f)
-
+def loadSettings():
+    try:
+        with open("settings.json") as f:
+            settings = json.load(f)
+            return settings
+    except Exception:
+        settings = {'speech_speed': 170, 'voice_number': 0, 'music_folder': ''}
+        with open('settings.json', 'w') as f:
+                json.dump(settings, f)
+        return settings
 
 def googleSearch(response):
     webbrowser.open(response)
@@ -23,13 +25,12 @@ def youtubeSearch(response):
 
 def downloadMusic(response): # handeling the pass but its done
     video = YouTube(response)
-    print(response)
     audio = video.streams.last()
-    musicPath = os.path.expanduser("D:\IMages\RED")
-    audio.download(musicPath)
-    
-
-
+    musicPath = os.path.expanduser("~/Desktop")
+    try:
+        audio.download(musicPath)
+    except Exception:
+        pass
 
 mappings = {
     "2" : googleSearch,
