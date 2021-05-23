fa12fc44b61bdef5329aa9706ec92c0dd695dd57
Mohameosama <moandosama1163@gmail.com>
2021-05-23 17:30:56 +0200

    added screenshot function to server

--- server/functions.py
+++ server/functions.py
@@ -79,7 +79,8 @@ def startWordProject(response, server, client):
     docName = server.getMessage(client)
     server.sendMessage(client,docName,5)
 
-      
+def screenShot(response, server, client):
+    server.sendMessage(client,response,6)
 
 mappings = {
     "random" : randomRange,
@@ -90,5 +91,6 @@ mappings = {
     "date" : getDate,
     "downloadmusic" : downloadMusic,
     "prayer" : prayerTime,
-    "Word" :startWordProject
+    "Word" :startWordProject,
+    "screenshot": screenShot
 }
