ba08f2cf7783faa06d07e13b18dfa842c081aa88
Mohameosama <moandosama1163@gmail.com>
2021-05-23 17:30:33 +0200

    added handling for screenshot flag

--- client/usersock.py
+++ client/usersock.py
@@ -59,6 +59,10 @@ class ClientSock():
         elif flag == 5:
             self.functions["5"](response)
             self.restartConnection()           
+        elif flag == 6:
+            self.functions["6"](response)
+            self.restartConnection()           
+            
             
 
 
