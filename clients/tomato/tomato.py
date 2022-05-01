6949ab5ee5391a05beef37d482fefa387d29af97
karim kohel <kareemkohel@gmail.com>
2022-05-02 01:01:08 +0200

    added comments

--- clients/tomato/tomato.py
+++ clients/tomato/tomato.py
@@ -10,8 +10,11 @@ while True:
     spr.waitForWakeupCall("tomato")
     while True:
         command = spr.listen()
+        # get json response
         response = handler.getResponse(command)
+        # speak the msg part of that json
         spr.speak(response['msg'])
+        # if json response has function code then execute said function
         if response["function"]:
             handler.handleFunction(response['function'])
 
